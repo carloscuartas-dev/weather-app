1d88c268cae203e3c9bfb4219a8c1873cf22f39a
Carlos Cuartas Marin <ccuartas@hiberus.com>
2023-05-10 18:10:29 +0200

    Commit 202305101810

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?"
+    const url = "https://api.openweathermap.org/data/3.0/onecall?"
     const lat= "lat=42.81&"
     const lon= "lon=-1.64&"
     const apiOptions = "units=metric&exclude=minutely,alerts&";
-    const appid= "appid=d37ce70b714076ca5396f1698c461eea"
+    const appid= "appid=d37ce70b714076ca5396f1698c461eea" 
     const queryUrl = url + lat + lon + apiOptions + appid;
-    /*
-    const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Paris';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'dd609a2733mshb6bd07643428abcp18de13jsn2b36d7ea063f',
-            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-        }
-    };
-    */
+    
 
     fetch (queryUrl) 
         .then(response => response.json())
         .then(data => {
             console.log(data);
             // Weather main data
-            const main = data.current.weather[0].main;
-            const description = data.current.weather[0].description;
-            const temp = Math.round(data.current.temp);
-            const pressure = data.current.pressure;
-            const humidity = data.current.humidity;
+            let description = data.current.weather[0].description;
+            let temp = Math.round(data.current.temp);
+            let pressure = data.current.pressure;
+            let humidity = data.current.humidity;
+            let name = "Pamplona";
+
+            document.getElementById("wrapper-name").innerHTML = "Pamplona";
+            document.getElementById("wrapper-description").innerHTML = description;
+            document.getElementById("wrapper-temp").innerHTML = temp + "°C";
+            document.getElementById("wrapper-pressure").innerHTML = pressure;
+            document.getElementById("wrapper-humidity").innerHTML = humidity + "°C";
+
         });
     
     console.log("DOM fully loaded and parsed!");
